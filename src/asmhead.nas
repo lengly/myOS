20f1b6bf771697db62e4acc591cc7cdf264b0e6e
lengly <liu_yuda@163.com>
2015-04-21 19:42:54 +0800

    day 8

--- src/asmhead.nas
+++ src/asmhead.nas
@@ -29,12 +29,19 @@ VRAM	EQU		0x0ff8			; 图像缓冲区的开始地址
 		INT		0x16 			; keyboard BIOS
 		MOV		[LEDS],AL
 
+; PIC关闭一切中断
+;	根据AT兼容机的规格，如果要初始化PIC
+;	必须在CLI之前进行，否则有时会挂起
+;	随后进行PIC的初始化
+
 		MOV		AL,0xff
 		OUT		0x21,AL
-		NOP
+		NOP						; 如果连续执行OUT指令，有些机种会无法正常运行
 		OUT		0xa1,AL
 
-		CLI
+		CLI						; 禁止CPU级别的中断
+
+; 为了让CPU能够访问1MB以上的内存空间，设定A20GATE
 
 		CALL	waitkbdout
 		MOV		AL,0xd1
@@ -44,61 +51,72 @@ VRAM	EQU		0x0ff8			; 图像缓冲区的开始地址
 		OUT		0x60,AL
 		CALL	waitkbdout
 
+; 切换到保护模式
 
+[INSTRSET "i486p"]				; "想要使用486指令"的叙述
 
-[INSTRSET "i486p"]
-
-		LGDT	[GDTR0]
+		LGDT	[GDTR0]			; 设定临时GDT
 		MOV		EAX,CR0
-		AND		EAX,0x7fffffff
-		OR		EAX,0x00000001
+		AND		EAX,0x7fffffff	; 设bit31为0 (为了禁止颁)
+		OR		EAX,0x00000001	; 设bit0为1 (为了切换到保护模式)
 		MOV		CR0,EAX
 		JMP		pipelineflush
 pipelineflush:
-		MOV		AX,1*8
+		MOV		AX,1*8			;  可读写的段 32bit
 		MOV		DS,AX
 		MOV		ES,AX
 		MOV		FS,AX
 		MOV		GS,AX
 		MOV		SS,AX
 
+; bootpack的转送
 
-		MOV		ESI,bootpack
-		MOV		EDI,BOTPAK
+		MOV		ESI,bootpack	; 转送源
+		MOV		EDI,BOTPAK		; 转送目的地
 		MOV		ECX,512*1024/4
 		CALL	memcpy
 
-		MOV		ESI,0x7c00
-		MOV		EDI,DSKCAC
+; 磁盘数据最终转送到它本来的位置去
+
+; 首先从启动扇区开始
+
+		MOV		ESI,0x7c00		; 转送源
+		MOV		EDI,DSKCAC		; 转送目的地
 		MOV		ECX,512/4
 		CALL	memcpy
 
-		MOV		ESI,DSKCAC0+512
-		MOV		EDI,DSKCAC+512
+; 所有剩下的
+
+		MOV		ESI,DSKCAC0+512	; 转送源
+		MOV		EDI,DSKCAC+512	; 转送目的地
 		MOV		ECX,0
 		MOV		CL,BYTE [CYLS]
-		IMUL	ECX,512*18*2/4
-		SUB		ECX,512/4
+		IMUL	ECX,512*18*2/4	; 从柱面数变为字节数/4
+		SUB		ECX,512/4		; 减去 IPL
 		CALL	memcpy
 
+; 必须由asmhead来完成的工作，至此全部完毕
+;	以后就交由bootpack来完成
+
+; bootpack的启动
 
 		MOV		EBX,BOTPAK
 		MOV		ECX,[EBX+16]
-		ADD		ECX,3
-		SHR		ECX,2
-		JZ		skip
-		MOV		ESI,[EBX+20]
+		ADD		ECX,3			; ECX += 3;
+		SHR		ECX,2			; ECX /= 4;
+		JZ		skip			; 没有要转送的东西时
+		MOV		ESI,[EBX+20]	; 转送源
 		ADD		ESI,EBX
-		MOV		EDI,[EBX+12]
+		MOV		EDI,[EBX+12]	; 转送目的地
 		CALL	memcpy
 skip:
-		MOV		ESP,[EBX+12]
+		MOV		ESP,[EBX+12]	; 栈初始值
 		JMP		DWORD 2*8:0x0000001b
 
 waitkbdout:
 		IN		 AL,0x64
 		AND		 AL,0x02
-		JNZ		waitkbdout
+		JNZ		waitkbdout		; AND的结果如果不是0，就跳到waitkbdout
 		RET
 
 memcpy:
@@ -107,15 +125,14 @@ memcpy:
 		MOV		[EDI],EAX
 		ADD		EDI,4
 		SUB		ECX,1
-		JNZ		memcpy
+		JNZ		memcpy			; 减法运算的结果如果不是0，就跳转到memcpy
 		RET
-
 
 		ALIGNB	16
 GDT0:
-		RESB	8
-		DW		0xffff,0x0000,0x9200,0x00cf
-		DW		0xffff,0x0000,0x9a28,0x0047
+		RESB	8				; NULL selector
+		DW		0xffff,0x0000,0x9200,0x00cf	; 可以读写的段 (segment) 32bit
+		DW		0xffff,0x0000,0x9a28,0x0047	; 可以执行的段 (segment) 32bit（bootpack用）
 
 		DW		0
 GDTR0:
--- src/bootpack.c
+++ src/bootpack.c
@@ -1,16 +1,23 @@
 #include "bootpack.h"
 #include <stdio.h>
 
+struct MOUSE_DEC {
+	unsigned char buf[3], phase;
+	int x, y, btn;
+};
+
 extern struct FIFO8 keyfifo;
 extern struct FIFO8 mousefifo;
-void enable_mouse(void);
+void enable_mouse(struct MOUSE_DEC *mdec);
 void init_keyboard(void);
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char data);
 
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
 	char s[40], mcursor[256], keybuf[32], mousebuf[128];
 	int mx, my, i;
+	struct MOUSE_DEC mdec;
 	
 	init_gdtidt();
 	init_pic();
@@ -33,7 +40,7 @@ void HariMain(void)
 	sprintf(s, "(%d %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 	
-	enable_mouse();
+	enable_mouse(&mdec);
 	
 	for(;;) {
 		io_cli();
@@ -49,9 +56,41 @@ void HariMain(void)
 			} else if (fifo8_status(&mousefifo) != 0) {
 				i = fifo8_get(&mousefifo);
 				io_sti();
-				sprintf(s, "%02X", i);
-				boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 47, 31);
-				putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+				if (mouse_decode(&mdec, i) == 1) {
+				    /* 数据的3个字节都齐了 显示出来 */
+					sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+					if ((mdec.btn & 0x01) != 0) {
+						s[1] = 'L';
+					}
+					if ((mdec.btn & 0x02) != 0) {
+						s[3] = 'R';
+					}
+					if ((mdec.btn & 0x04) != 0) {
+						s[2] = 'C';
+					}
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 15*8 -1, 31);
+					putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+					/* 数据的3个字节都齐了 显示出来 */
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, mx, my, mx+15, my+15); /* 隐藏坐标 */
+					mx += mdec.x;
+					my += mdec.y;
+					if (mx < 0) {
+						mx = 0;
+					}
+					if (my < 0) {
+						my = 0;
+					}
+					if (mx > binfo->scrnx - 16) {
+						mx = binfo->scrnx - 16;
+					}
+					if (my > binfo->scrny - 16) {
+						my = binfo->scrny - 16;
+					}
+					sprintf(s, "(%3d, %3d)", mx, my);
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 0, 79, 15); /* 隐藏坐标 */
+					putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s); /* 显示坐标 */
+					putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16); /* 描画鼠标 */
+				}
 			}
 		}
 	}
@@ -86,12 +125,53 @@ void init_keyboard(void)
 #define KEYCMD_SENDTO_MOUSE		0xd4
 #define MOUSECMD_ENABLE			0xf4
 
-void enable_mouse(void)
+void enable_mouse(struct MOUSE_DEC *mdec)
 {
 	wait_KBC_sendready();
 	io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
 	wait_KBC_sendready();
 	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+	mdec->phase = 0;  /* 等待鼠标进入0xfa的状态 */
 	return;
+}
+
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char data)
+{
+	if (mdec->phase == 0) {
+		/* 等待鼠标的0xfa的阶段 */
+		if (data == 0xfa) {
+			mdec->phase = 1;
+		}
+		return 0;
+	} else if (mdec->phase == 1) {
+		/* 等待鼠标第一字节的阶段 */
+		if ((data & 0xc8) == 0x08) {
+			/* 如果第一字节正确 */
+			mdec->buf[0] = data;
+			mdec->phase = 2;
+		}
+		return 0;
+	} else if (mdec->phase == 2) {
+		/* 等待鼠标第二字节的阶段 */
+		mdec->buf[1] = data;
+		mdec->phase = 3;
+		return 0;
+	} else if (mdec->phase == 3) {
+		/* 等待鼠标第三字节的阶段 */
+		mdec->buf[2] = data;
+		mdec->phase = 1;	
+		mdec->btn = mdec->buf[0] & 0x07;
+		mdec->x = mdec->buf[1];
+		mdec->y = mdec->buf[2];
+		if ((mdec->buf[0] & 0x10) != 0) {
+			mdec->x |= 0xffffff00;
+		}
+		if ((mdec->buf[0] & 0x20) != 0) {
+			mdec->y |= 0xffffff00;
+		}
+		mdec->y = - mdec->y; /* 鼠标y轴方向与画面方向相反 */
+		return 1;
+	}
+	return -1;
 }
 
