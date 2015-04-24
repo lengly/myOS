e9e4ce2b9da2f6f0f737c3e294d405408fdb0aac
lengly <liu_yuda@163.com>
2015-04-24 20:25:32 +0800

    day 10

--- src/bootpack.c
+++ src/bootpack.c
@@ -1,32 +1,17 @@
 #include "bootpack.h"
 #include <stdio.h>
 
-#define MEMMAN_FREES	4090  /* 大约32KB */
-#define MEMMAN_ADDR		0x003c0000
-
-struct FREEINFO {   /* 可用信息 */
-	unsigned int addr, size;
-};
-
-struct MEMMAN {  /* 内存管理 */
-	int frees, maxfrees, lostsize, losts;
-	struct FREEINFO free[MEMMAN_FREES];
-};
-
-unsigned int memtest(unsigned int start, unsigned int end);
-void memman_init(struct MEMMAN *man);
-unsigned int memman_total(struct MEMMAN *man);
-unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
-int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
-
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
-	char s[40], mcursor[256], keybuf[32], mousebuf[128];
+	char s[40], keybuf[32], mousebuf[128];
 	int mx, my, i;
 	unsigned int memtotal;
 	struct MOUSE_DEC mdec;
 	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
+	struct SHTCTL *shtctl;
+	struct SHEET *sht_back, *sht_mouse;
+	unsigned char *buf_back, buf_mouse[256];
 	
 	init_gdtidt();
 	init_pic();
@@ -44,24 +29,27 @@ void HariMain(void)
 	memman_free(memman, 0x00001000, 0x0009e00);
 	memman_free(memman, 0x00400000, memtotal - 0x00400000);
 	
-	
 	init_palette();
-	init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
-	
+	shtctl = shtctl_init(memman, binfo->vram, binfo->scrnx, binfo->scrny);
+	sht_back = sheet_alloc(shtctl);
+	sht_mouse = sheet_alloc(shtctl);
+	buf_back = (unsigned char *) memman_alloc_4k(memman, binfo->scrnx * binfo->scrny);
+	sheet_setbuf(sht_back, buf_back, binfo->scrnx, binfo->scrny, -1);
+	sheet_setbuf(sht_mouse, buf_mouse, 16, 16, 99);
+	init_screen8(buf_back, binfo->scrnx, binfo->scrny);
+	init_mouse_cursor8(buf_mouse, 99);
+	sheet_slide(shtctl, sht_back, 0, 0);
 	mx = (binfo->scrnx - 16) / 2;
 	my = (binfo->scrny - 28 - 16) / 2;
-	init_mouse_cursor8(mcursor, COL8_008484);
-	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+	sheet_slide(shtctl, sht_mouse, mx, my);
+	sheet_updown(shtctl, sht_back, 0);
+	sheet_updown(shtctl, sht_mouse, 1);
 	sprintf(s, "(%d %d)", mx, my);
-	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
-	
-	i = memtest(0x00400000, 0xbfffffff) / (1024 * 1024);
-	sprintf(s, "memory %dMB", i);
-	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
-	
+	putfonts8_asc(buf_back, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 	sprintf(s, "memory %dMB   free : %dKB",
 			memtotal / (1024 * 1024), memman_total(memman) / 1024);
-	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
+	putfonts8_asc(buf_back, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
+	sheet_refresh(shtctl, sht_back, 0, 0, binfo->scrnx, 48);
 	
 	for(;;) {
 		io_cli();
@@ -72,8 +60,9 @@ void HariMain(void)
 				i = fifo8_get(&keyfifo);
 				io_sti();
 				sprintf(s, "%02X", i);
-				boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
-				putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+				boxfill8(buf_back, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
+				putfonts8_asc(buf_back, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+				sheet_refresh(shtctl, sht_back, 0, 16, 16, 32);
 			} else if (fifo8_status(&mousefifo) != 0) {
 				i = fifo8_get(&mousefifo);
 				io_sti();
@@ -89,10 +78,10 @@ void HariMain(void)
 					if ((mdec.btn & 0x04) != 0) {
 						s[2] = 'C';
 					}
-					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 15*8 -1, 31);
-					putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+					boxfill8(buf_back, binfo->scrnx, COL8_008484, 32, 16, 32 + 15*8 -1, 31);
+					putfonts8_asc(buf_back, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+					sheet_refresh(shtctl, sht_back, 32, 16, 32 + 15 * 8, 32);
 					/* 数据的3个字节都齐了 显示出来 */
-					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, mx, my, mx+15, my+15); /* 隐藏坐标 */
 					mx += mdec.x / 100;
 					my += mdec.y / 100;
 					if (mx < 0) {
@@ -108,153 +97,13 @@ void HariMain(void)
 						my = binfo->scrny - 16;
 					}
 					sprintf(s, "(%3d, %3d)", mx, my);
-					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 0, 79, 15); /* 隐藏坐标 */
-					putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s); /* 显示坐标 */
-					putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16); /* 描画鼠标 */
+					boxfill8(buf_back, binfo->scrnx, COL8_008484, 0, 0, 79, 15); /* 隐藏坐标 */
+					putfonts8_asc(buf_back, binfo->scrnx, 0, 0, COL8_FFFFFF, s); /* 显示坐标 */
+					sheet_refresh(shtctl, sht_back, 0, 0, 80, 16);
+					sheet_slide(shtctl, sht_mouse, mx, my);
 				}
 			}
 		}
 	}
-}
-
-#define EFLAGS_AC_BIT		0x00040000
-#define CR0_CACHE_DISABLE	0x60000000
-
-unsigned int memtest(unsigned int start, unsigned int end)
-{
-	char flag486 = 0;
-	unsigned int eflag, cr0, i;
-	
-	/* 确认CPU是386还是486以上的 */
-	eflag = io_load_eflags();
-	eflag |= EFLAGS_AC_BIT; /* AC-bit = 1 */
-	io_store_eflags(eflag);
-	eflag = io_load_eflags();
-	if ((eflag & EFLAGS_AC_BIT) != 0) {  /* 如果是386，即使设定AC＝1，AC的值还会自动回到0 */
-		flag486 = 1;
-	}
-	eflag &= ~EFLAGS_AC_BIT;
-	io_store_eflags(eflag);
-	
-	if (flag486 != 0) {
-		cr0 = load_cr0();
-		cr0 |= CR0_CACHE_DISABLE; /* 禁止缓存 */
-		store_cr0(cr0);
-	}
-	
-	i = memtest_sub(start, end);
-	
-	if (flag486 != 0) {
-		cr0 = load_cr0();
-		cr0 &= ~CR0_CACHE_DISABLE; /* 允许缓存 */
-		store_cr0(cr0);
-	}
-	
-	return i;
-}
-
-void memman_init(struct MEMMAN *man)
-{
-	man->frees = 0;		/* 可用信息数目 */
-	man->maxfrees = 0;	/* 用于观察可用状况：frees的最大值 */
-	man->lostsize = 0;	/* 解放失败的内存的大小综合 */
-	man->losts = 0;		/* 解放失败次数 */
-	return;
-}
-
-unsigned int memman_total(struct MEMMAN *man)
-/* 报告生剩余空间大小 */
-{
-	unsigned int i, t = 0;
-	for (i = 0; i < man->frees; i++) {
-		t += man->free[i].size;
-	}
-	return t;
-}
-
-unsigned int memman_alloc(struct MEMMAN *man, unsigned int size)
-/* 分配 */
-{
-	unsigned int i, a;
-	for(i = 0; i < man->frees; i++) {
-		if (man->free[i].size >= size) {
-			/* 找到了足够大的内存 */
-			a = man->free[i].addr;
-			man->free[i].addr += size;
-			man->free[i].size -= size;
-			if (man->free[i].size == 0) {
-				/* 如果free[i]变成了0，就减掉一条可用信息 */
-				man->frees--;
-				for(; i < man->frees; i++) {
-					man->free[i] = man->free[i+1];
-				}
-			}
-			return a;
-		}
-	}
-	return 0; /* 没有可用空间 */
-}
-
-int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size)
-/* 释放 */
-{
-	int i, j;
-	/* 为了便于归纳内存，将free[]按照addr的顺序排列 */
-	/* 所以，先决定应该放在哪里 */
-	for(i = 0; i < man->frees; i++) {
-		if (man->free[i].addr > addr) {
-			break;
-		}
-	}
-	/* free[i - 1].addr < addr < free[i].addr */
-	if (i > 0) {
-		/* 前面有可用内存 */
-		if (man->free[i-1].addr + man->free[i-1].size == addr) {
-			/* 可以与前面的可用内存归纳到一起 */
-			man->free[i-1].size += size;
-			if (i < man->frees) {
-				/* 后面也有 */
-				if (addr + size == man->free[i].addr) {
-					/* 也可以与后面的可用内存归纳到一起 */
-					man->free[i-1].size += man->free[i].size;
-					/* man->free[i]删除 */
-					/* free[i]变成0后归纳到前面去 */
-					man->frees--;
-					for(; i < man->frees; i++) {
-						man->free[i] = man->free[i+1]; /* 结构体赋值 */
-					}
-				}
-			}
-			return 0; /* 成功完成 */
-		}
-	}
-	/* 不能与前面的可用空间归纳到一起 */
-	if (i < man->frees) {
-		/* 后面还有 */
-		if (addr + size == man->free[i].addr) {
-			/* 可以与后面的内容归纳到一起 */
-			man->free[i].addr = addr;
-			man->free[i].size += size;
-			return 0; /* 成功完成 */
-		}
-	}
-	/* 既不能与前面归纳到一起，也不能与后面归纳到一起 */
-	if (man->frees < MEMMAN_FREES) {
-		/* free[i]之后的，向后移动，腾出一点可用空间 */
-		for(j = man->frees; j > i; j--) {
-			man->free[j] = man->free[j-1];
-		}
-		man->frees++;
-		if (man->maxfrees < man->frees) {
-			man->maxfrees = man->frees; /* 更新最大值 */
-		}
-		man->free[i].addr = addr;
-		man->free[i].size = size;
-		return 0; /* 成功完成 */
-	}
-	/* 不能往后移动 */
-	man->losts++;
-	man->lostsize += size;
-	return -1; /* 失敗 */
 }
 
--- src/graphic.c
+++ src/graphic.c
@@ -50,7 +50,7 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, i
 	return;	
 }
 
-void init_screen(char *vram, int x, int y)
+void init_screen8(char *vram, int x, int y)
 {
 	boxfill8(vram, x, COL8_008484,  0,     0,      x -  1, y - 29);
 	boxfill8(vram, x, COL8_C6C6C6,  0,     y - 28, x -  1, y - 28);
